f1a3c865988157baf83cf14836ee8d20974fe89c
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-03-30 22:38:48 -0700

    adds functionality to contract

--- casino-ethereum/contracts/Casino.sol
+++ casino-ethereum/contracts/Casino.sol
@@ -15,12 +15,34 @@ contract Casino {
     
     //The address of the player and => the user info mapping(address => Player) public playerInfo;
 
-
-    function Casino() public {
+    //this is the constructor because it has the same name as that of the contract and we use it to set up the owner of that contract
+    function Casino(uint256 _minimumBet) public {
         owner = msg.sender;
+        if(_minimumBet != 0) minimumBet = _minimumBet
     }
 
     function kill() public {
         if(msg.sender == owner) selfdestruct(owner);
     }
+
+    //makes sure that the player only plays once per game
+    function checkPlayerExists(address player) public constant returns(bool){
+        for(uint256 i = 0; i < players.length; i++){
+            if(players[i] == player) return true;
+        }
+        return false;
+    }
+
+    // To bet for a number between 1 and 10 both inclusive
+   function bet(uint256 numberSelected) public payable {
+      require(!checkPlayerExists(msg.sender));
+      require(numberSelected >= 1 && numberSelected <= 10);
+      require(msg.value >= minimumBet);
+
+      playerInfo[msg.sender].amountBet = msg.value;
+      playerInfo[msg.sender].numberSelected = numberSelected;
+      numberOfBets++;
+      players.push(msg.sender);
+      totalBet += msg.value;
+   }
 }
